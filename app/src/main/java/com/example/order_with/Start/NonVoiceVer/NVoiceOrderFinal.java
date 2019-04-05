f5565997973a3c2d3308d4a4d9b2191c05a90e08
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-04-05 15:40:19 +0900

    NVoiceFinal에 onpause추가

--- app/src/main/java/com/example/order_with/Start/NonVoiceVer/NVoiceOrderFinal.java
+++ app/src/main/java/com/example/order_with/Start/NonVoiceVer/NVoiceOrderFinal.java
@@ -1,31 +1,19 @@
 package com.example.order_with.Start.NonVoiceVer;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.os.Handler;
-import android.os.Parcelable;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Button;
-
 import com.example.order_with.R;
 import com.example.order_with.menuItem.Menu;
 import com.example.order_with.menuItem.MenuAdapter;
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Dictionary;
-import java.util.Enumeration;
 import java.util.Locale;
-
 import static android.speech.tts.TextToSpeech.ERROR;
 
-
 public class NVoiceOrderFinal extends AppCompatActivity {
-    private MenuAdapter mAdapter;
     private MenuAdapter mAdapter;
     private TextToSpeech tts;
     String startVoice;
@@ -45,6 +33,15 @@ public class NVoiceOrderFinal extends AppCompatActivity {
         ArrayList<Menu> menuList = intent.getParcelableArrayListExtra("clickedItem");
         menuList = intent.getParcelableArrayListExtra("clickedItem");
 
+        String voice1 = "장바구니에는";
+        String voice2 = "가 있습니다. 맞으면 결제, 틀리면 다시를 말해주세요.";
+        String menu = " ";
+        for (int i = 0; i < menuList.size(); i++) {
+            menu += menuList.get(i).getTitle();
+        }
+        startVoice = voice1 + menu + voice2;
+
+
         MenuAdapter adapter = new MenuAdapter(menuList);
         adapter.notifyItemInserted(0);
         recyclerView.setAdapter(adapter);
@@ -53,7 +50,6 @@ public class NVoiceOrderFinal extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        MakeString();
         VoiceStarting();
         delayHandler = new Handler();
         delayHandler.postDelayed(new Runnable() {
@@ -62,15 +58,6 @@ public class NVoiceOrderFinal extends AppCompatActivity {
                 //StartSTT();
             }
         }, 8000);
-    }
-    public void MakeString() {
-        String voice1 = "장바구니에는";
-        String voice2 = "가 있습니다. 맞으면 결제, 틀리면 다시를 말해주세요.";
-        String menu = " ";
-        for (int i = 0; i < menuList.size(); i++){
-            menu += menuList.get(i).getTitle();
-        }
-        startVoice = voice1 + menu + voice2;
     }
 
     private void VoiceStarting() {
@@ -85,4 +72,11 @@ public class NVoiceOrderFinal extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        tts.stop();
+        tts.shutdown();
+        delayHandler.removeMessages(0);
+    }
 }
